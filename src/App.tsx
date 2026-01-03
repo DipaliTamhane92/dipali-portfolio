bfa96b811061d57318e51c347f34dda5b7658595
Dipali Tamhane <dipalitamhane711@gmail.com>
2026-01-03 11:13:23 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+
+function App() {
+
+  return (
+    <>
+    <header>
+        <div>
+          Name:
+          Dipali Tamhane
+        </div>
+        <div>
+          Title:
+          Frontend Developer
+        </div>
+    </header>
+    <main>
+        <div>
+          Tagline:
+          Frontend developer with 5+ years of experience building responsive web application using React, Javascript, Css, bootstrap, Typescript.
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+    </>
+  )
+}
+
+export default App
